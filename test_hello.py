3f13c830e04ec0059c6f3ce459d1db274fbbbc32
zartwilly <wehounou@gmail.com>
2023-08-30 14:50:11 +0000

    adding initial layout of python project

---
+++ test_hello.py
@@ -0,0 +1,10 @@
+from hello import more_hello, more_goodbye
+
+
+def test_more_hello():
+    assert "hi" == more_hello()
+
+
+def test_more_goodbye():
+    # assert "hi" == more_goodbye()
+    assert "bye" == more_goodbye()
